67ac126100c4cea0d4798884261da4fc806a7ce9
Daniel King <dking@broadinstitute.org>
2017-11-06 11:00:41 -0500

    add verification command and a continuation message

--- git-db.sh
+++ git-db.sh
@@ -5,22 +5,32 @@ set -ex
 # git-db newroot oldroot b1 b2 b3 ...
 # b3 depends on b2, b2 depends on b1, b1 previously depended on oldroot
 
-if [[ $# -lt 3 ]]
+if [[ $# -lt 4 ]]
 then
-    echo "USAGE: git-db newroot oldroot b1 ..."
+    echo "USAGE: git-db verify newroot oldroot b1 ..."
     exit -1
 fi
 
-NEWROOT="$1"
-OLDROOT=$(git rev-parse "$2")
-shift 2
+VERIFY="$1"
+NEWROOT="$2"
+OLDROOT=$(git rev-parse "$3")
+shift 3
+
+function continuation() {
+    echo "in process " $NEWROOT $OLDROOT $CURRENT_NAME
+    echo "next " $CURRENT_NAME $CURRENT_HASH $*
+}
+
+trap continuation SIGINT SIGTERM
 
 while [[ $# -gt 0 ]]
 do
     CURRENT_NAME="$1"
+    shift
     CURRENT_HASH=$(git rev-parse "${CURRENT_NAME}")
     git rebase --onto "${NEWROOT}" "${OLDROOT}" "${CURRENT_NAME}"
+    eval $VERIFY
     NEWROOT="${CURRENT_NAME}"
     OLDROOT="${CURRENT_HASH}"
-    shift
 done
+trap - SIGINT SIGTERM
